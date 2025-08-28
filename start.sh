b2b0d99ec41b1a58db515b45ae6b7fa6fc8afb2f
NQAnh01 <ngquocanh280101@gmail.com>
2025-08-28 22:30:26 +0700

    Remove .sh scripts from tracking

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -eu
-
-echo "🚀 Starting containers..."
-docker compose up -d
-
-echo "✅ Containers are up. Use './stop.sh' to stop them."
--- stop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -eu
-
-echo "🛑 Stopping containers..."
-docker compose down --remove-orphans
-
-echo "✅ Containers stopped."
